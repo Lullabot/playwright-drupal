5b203ad5855b6a6b4e1e679fb8b92719a2ce040a
Christian Lopez Espinola <penyaskito@computer.org>
2025-02-26 22:42:22 +0100

    Clean duplicated files.

--- lib/global-setup.d.ts
+++
@@ -1,9 +0,0 @@
-import { FullConfig } from "@playwright/test";
-/**
- * Global setup callback for Playwright
- *
- * https://playwright.dev/docs/test-global-setup-teardown#option-2-configure-globalsetup-and-globalteardown
- * @param config
- */
-declare function globalSetup(config: FullConfig): void;
-export default globalSetup;
